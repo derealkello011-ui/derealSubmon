d7ace5431b3cd40c37f71135cb3b2551d41759ef
d3r3alk3ll0 <derealkello011@gmail.com>
2026-09-16 13:39:00 +0000

    feat: update layout and styles with custom fonts and improved theme variables

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -4,10 +4,16 @@ import { SafeAreaView } from "react-native-safe-area-context";
  
 export default function App() {
   return (
-    <SafeAreaView className="flex-1 justify-center items-center bg-white">
-      <Text className="font-bold text-blue-500 text-xl">
+    <SafeAreaView className="flex-1 bg-background p-5">
+
+      <Text className="py-20 font-sans-bold text-7xl">
         Welcome to Nativewind!
       </Text>
+
+      <Text className="font-bold text-7xl">
+        Welcome to Nativewind!
+      </Text>
+
     </SafeAreaView>
   );
 }
--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,10 +1,26 @@
 import { useFonts } from 'expo-font';
-import { Stack } from "expo-router";
+import { SplashScreen, Stack } from "expo-router";
+import { useEffect } from 'react';
 
 export default function RootLayout() {
   const [ fontsLoaded ] = useFonts( {
-      
+    'sans-regular': require( '@/assets/fonts/Plus_Jakarta_Sans/static/PlusJakartaSans-Regular.ttf' ),
+    'sans-bold': require('@/assets/fonts/Plus_Jakarta_Sans/static/PlusJakartaSans-Bold.ttf'),
+    'sans-medium': require('@/assets/fonts/Plus_Jakarta_Sans/static/PlusJakartaSans-Medium.ttf'),
+    'sans-semibold': require('@/assets/fonts/Plus_Jakarta_Sans/static/PlusJakartaSans-SemiBold.ttf'),
+    'sans-extrabold': require('@/assets/fonts/Plus_Jakarta_Sans/static/PlusJakartaSans-ExtraBold.ttf'),
+    'sans-light': require( '@/assets/fonts/Plus_Jakarta_Sans/static/PlusJakartaSans-Light.ttf' ),
+    'sans-italic' : require('@/assets/fonts/Plus_Jakarta_Sans/static/PlusJakartaSans-Italic.ttf')
   } );
+  
+  useEffect( () => { 
+    if ( fontsLoaded ) {
+      SplashScreen.hideAsync();
+    }
+  }, [ fontsLoaded ] );
+  
+  if ( !fontsLoaded ) return null;
+
   return <Stack screenOptions={{
     headerShown: false,
     contentStyle: {backgroundColor: '#ffffff'}
