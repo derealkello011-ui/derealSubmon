5166803a578c49a13ace172b328d3ee759361779
d3r3alk3ll0 <derealkello011@gmail.com>
2026-09-16 14:21:12 +0000

    feat: add Home text to the header in the main app view

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,11 +1,13 @@
 import "@/global.css";
-import { View } from "react-native";
+import { Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
  
 export default function App() {
   return (
     <SafeAreaView className="flex-1 bg-background p-5">
-      <View className="home-header" ></View>
+      <View className="home-header" >
+        <Text>Home</Text>
+      </View>
 
     </SafeAreaView>
   );
