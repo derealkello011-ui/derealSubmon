4028c74260410f4c4399633d9067dda89b56e078
d3r3alk3ll0 <derealkello011@gmail.com>
2026-09-16 13:55:10 +0000

    feat: add onboarding link and adjust text sizes for consistency

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,4 +1,5 @@
 import "@/global.css";
+import { Link } from "expo-router";
 import { Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
  
@@ -6,13 +7,17 @@ export default function App() {
   return (
     <SafeAreaView className="flex-1 bg-background p-5">
 
-      <Text className="py-20 font-sans-bold text-7xl">
+      <Text className="py-20 font-sans-bold text-6xl">
         Welcome to Nativewind!
       </Text>
 
-      <Text className="font-bold text-7xl">
+      <Text className="font-bold text-6xl">
         Welcome to Nativewind!
       </Text>
+
+      <Link href='/onboarding'
+        className="bg-primary mt-4 p-4 rounded font-semibold text-white"
+      >Go to onboarding</Link>
 
     </SafeAreaView>
   );
